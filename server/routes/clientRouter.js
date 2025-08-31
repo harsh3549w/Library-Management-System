d04adbdfd6f07cfaa8b4b19f5689a220c289fe41
Akash Pandey <akashpandeyofficialo05@gmail.com>
2025-08-31 15:12:46 +0530

    Remove node_modules from client and server folders

--- server/routes/clientRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getClientImages } from '../controllers/clientController.js';
-
-const router = express.Router();
-
-router.get('/images', getClientImages);
-
-export default router;
