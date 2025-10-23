0ff60e160369d94bf500857fce655620fb69269d
Rohan <messilionelmesi289@gmail.com>
2025-10-23 15:44:49 +0530

    Fix: Mark ErrorHandler errors as operational to return correct HTTP status codes (400 instead of 500)

--- server/middlewares/errorMiddlewares.js
+++ server/middlewares/errorMiddlewares.js
@@ -5,6 +5,10 @@ class ErrorHandler extends Error {
     constructor(message, statusCode) {
       super(message);
       this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+      this.isOperational = true; // Mark as operational error
+      
+      Error.captureStackTrace(this, this.constructor);
     }
   }
   
