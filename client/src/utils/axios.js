75d6427134cdcc1ec97ccd667537e1f791478a92
Rohan <messilionelmesi289@gmail.com>
2025-10-26 00:26:22 +0530

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { store } from '../store/store'
+import { logout } from '../store/slices/authSlice'
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1'
 
@@ -25,11 +27,17 @@ axiosInstance.interceptors.request.use(
 // Handle response errors
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     if (error.response?.status === 401) {
-      // Unauthorized - clear token and redirect to login
+      // Unauthorized - clear token and dispatch logout
       localStorage.removeItem('token')
-      window.location.href = '/login'
+      
+      // Dispatch logout action to clear Redux state
+      // This will trigger the logout reducer and redirect will be handled by React Router
+      store.dispatch(logout())
+      
+      // Don't use window.location.href here as it causes issues with React Router
+      // The ProtectedRoute component will handle the redirect to /login
     }
     return Promise.reject(error)
   }
